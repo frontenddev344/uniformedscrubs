54cea0b8ef2e1cf89a129641ee5ebacb1a35cbdb
nishant <nd@int.iqsay.com>
2024-09-30 20:21:32 +0530

    updated

--- js/custom.js
+++ js/custom.js
@@ -1,3 +1,15 @@
+$(document).ready(function(){
+  $(".menu-toggle").click(function(){
+    $("body").addClass("toggle");
+  });
+});
+
+$(document).ready(function(){
+  $(".closed-menu").click(function(){
+    $("body").removeClass("toggle");
+  });
+});
+
 $('.slider').slick({
     autoplay: true,
     speed: 800,
